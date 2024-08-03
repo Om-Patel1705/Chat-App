3b6a58c8bab0f9921a255b1b959789e5e05e5c61
Nishtha-8104 <143199132+Nishtha-8104@users.noreply.github.com>
2024-08-03 10:28:23 +0530

    Update messageRoutes.js

--- server/Routes/messageRoutes.js
+++ server/Routes/messageRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const protect = require("../middleWare/authMiddleware");
-const { addMessage ,allMessages} = require("../controller/messages");
+const { addMessage ,allMessages,deleteMessage} = require("../controller/messages");
 
 
 const router = express.Router();
 
  router.route('/').post(protect,addMessage);
  router.route('/all').post(protect,allMessages);
-
+ router.route('/delete').post(protect,deleteMessage);
 
 
 module.exports=router;
