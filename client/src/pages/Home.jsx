7582b514c6a467555c5b30e3eb2e35f0d15fa973
Om-Patel1705 <122296443+Om-Patel1705@users.noreply.github.com>
2025-12-01 23:33:30 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
         textAlign={"center"}
       >
         {/* fontFamily={"work sans"} */}
-        <Text fontSize={"3xl"} fontFamily={"Nunito"} >Chat-App-Cloud-Uploaded</Text>
+        <Text fontSize={"3xl"} fontFamily={"Nunito"} >Chat-App-Cloud-Uploaded-2</Text>
       </Box>
       <Divider orientation='horizontal' />
       <Box
